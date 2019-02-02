796dfc4af9d6fdc263019824a3bf4bc019193c8f
sb6998 <39883172+sb6998@users.noreply.github.com>
2019-02-02 10:26:45 +0530

    Add files via upload

---
+++ __main__.py
@@ -0,0 +1,23 @@
+"""
+Created on Fri Feb  1 18:41:35 2019
+
+@author: saurabh
+"""
+
+import requests, zipfile, io, os
+
+def main():
+    url = 'https://github.com/sb6998/delpro/archive/master.zip'
+    r = requests.get(url)
+    z = zipfile.ZipFile(io.BytesIO(r.content))
+    z.extractall()
+    l = []
+    for x in z.NameToInfo:
+        l.append(x)
+    d = str(l[0])+"/Otree"
+    os.system("pip install otree")
+    os.chdir(d)
+    os.system("otree devserver")
+    
+if __name__== '__main__':
+    main()
